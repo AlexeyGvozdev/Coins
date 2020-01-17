3b326a87168e30a3ee21e6420ee8bc85e554d147
AlexeyGvozdev <gvozdev.alexey95@mail.ru>
2020-01-17 23:13:54 +0400

    add fake error api

--- app/src/main/java/ru/sinx/coins/di/services/api/Api.kt
+++ app/src/main/java/ru/sinx/coins/di/services/api/Api.kt
@@ -1,9 +1,11 @@
 package ru.sinx.coins.di.services.api
 
 import kotlinx.coroutines.delay
+import retrofit2.HttpException
 import retrofit2.http.GET
 import retrofit2.http.Query
 import ru.sinx.coins.di.services.api.response.BidTop
+import java.lang.IllegalStateException
 
 interface Api {
 
@@ -13,7 +15,7 @@ interface Api {
     @GET("/")
     suspend fun fetchCurrency(): List<String>
 
-    class FakeApi() : Api {
+    class FakeApiCorrect() : Api {
         override suspend fun fetchPair(pair: String): BidTop {
             delay(1000)
             return BidTop(Math.random() * 100)
@@ -22,6 +24,16 @@ interface Api {
         override suspend fun fetchCurrency(): List<String> {
             delay(1000)
             return listOf("BTC", "RUB", "EUR", "XRP", "USD")
+        }
+    }
+
+    class FakeApiError() : Api {
+        override suspend fun fetchPair(pair: String): BidTop {
+            throw IllegalStateException("fakeApi exception fetchpair")
+        }
+
+        override suspend fun fetchCurrency(): List<String> {
+            throw IllegalStateException("fakeApi fetchCurrency")
         }
 
     }
